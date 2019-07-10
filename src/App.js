f4e2046fb2490e21f942e502f9fcb246731084ca
adri <a.lopez@a2wgames.com>
2019-07-10 20:58:51 +0200

    Tested read, insert and delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Note from './components/Note';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tasks: [],
+      handleTask: '',
+      api: 'http://localhost:4000/'
+    }
+  }
+
+  componentDidMount() {
+    this.getTasks()
+  }
+
+  async getTasks() {
+    await axios.get(this.state.api)
+    .then((res) => {
+      this.setState({ tasks: res.data })
+    })
+  }
+
+  async deleteTask(id, i) {
+    await axios.delete(this.state.api + id).then(console.log('deleted id = ' + id))
+    let tasks = this.state.tasks
+    tasks.splice (i, 1)
+    this.setState({ tasks })
+  }
+
+  async postTask() {
+    await axios.post(this.state.api, {
+      task: this.state.handleTask,
+      status: 1
+    }).then(console.log(this.state.handleTask + ' insertada en db campeon sigue asi te queremos!'))
+    await this.getTasks()
+    this.setState({ handleTask: '' })
+    this.textInput.focus()
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter'){
+      this.postTask()
+    }
+  }
+
+  render() {
+    let tasks = this.state.tasks.map((task, i) => {
+      return <Note key={i} text={task.task} delete={ () => this.deleteTask(task.id, i)}/>
+    })
+    return (
+      <div className="con">
+      <div className="header">TODO CRUD TESTING</div>
+      <div className="mb-5 pb-5">
+      {tasks}
+      </div>
+      <div 
+          className="btn"
+          //CUIDADO!!!!!!!!!! si defines el metodo sin el bind, se queda en bucle añadiendo filas vacias
+          //MUCH CARE!!!!!! if u insert the function postTask without bind method
+          //the app go to bucle and add empty rows to db, for me more than 1k in 6 seconds :O
+          onClick={this.postTask.bind(this)}
+          ref={((add) => this.add = add)}
+          >
+            +
+      </div>
+      <input
+        type="text"
+        className="textInput"
+        ref={((input) => {this.textInput = input})}
+        onChange={task => {
+          this.setState({
+            handleTask: task.target.value
+          })
+        }}
+        value={this.state.handleTask}
+        onKeyPress={this.handleKeyPress.bind(this)}
+      />  
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/server/server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mysql = require('mysql')
+
+const app = express()
+
+app.set('port', process.env.PORT || 4000)
+
+app.listen(app.get('port'), () => {
+    console.log('====================================');
+    console.log('server running correctly on port: ' + app.get('port'));
+    console.log('====================================');
+})
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mydb'
+})
+
+const queryGet = 'SELECT * FROM tasks'
+const queryGetSpecific = 'SELECT * FROM tasks WHERE id = ?'
+const queryInsert = 'INSERT INTO tasks set ?'
+const queryDelete = 'DELETE FROM tasks WHERE id = ?'
+const queryUpdate = 'UPDATE tasks set ? WHERE id = ?'
+
+app.get('/', async (req, res) => {
+    await pool.query(queryGet, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.get('/:id', async (req, res) => {
+    await pool.query(queryGetSpecific, [req.params.id], (err, rows) => {
+        res.send(rows)
+    })
+})
+
+app.delete('/:id', async (req, res) => {
+    await pool.query(queryDelete, [req.params.id], () => {
+        res.send('deleted!')
+    })
+})
+
+app.post('/', async (req, res) => {
+    const { task, status } = req.body
+    const newTask = {
+        task,
+        status
+    }
+    await pool.query(queryInsert, [newTask], () => {
+        res.send('inserted!')
+    })
+})
+
+app.put('/:id', async (req, res) => {
+    const { task, status } = req.body
+    const updateTask = {
+        task,
+        status
+    }
+    await pool.query(queryUpdate, [updateTask, req.params.id], () => {
+        res.send('updated!')
+    })
+})
